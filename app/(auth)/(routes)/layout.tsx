840968eb15fc27fac34113514851939fcda20524
Yashwanth Chimata <ch.yashwanth1999@gmail.com>
2024-02-05 19:29:24 -0600

    modified:   app/(auth)/(routes)/layout.tsx 	modified:   app/layout.tsx 	modified:   package.json

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,13 +1,9 @@
-const AuthLayout = ({children}: {children: React.ReactNode}) => {
-    return (
-        
-        <div className="text-emerald-500 text-4xl font-bold">
-            Introducing Corddle
-            <div className="h-full flex items-center justify-center">
-                {children}
-            </div>
-        </div>
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return ( 
+      <div className="h-full flex items-center justify-center">
+        {children}
+      </div>
      );
-}
- 
-export default AuthLayout
+  }
+   
+  export default AuthLayout;
